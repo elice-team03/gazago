4758b3cdd9f9f4bb7b54646d07bdefa165a2d85a
lee joonyeong <cube1477@gmail.com>
2023-10-04 06:12:48 +0000

    feat: async-handler 추가 및 백엔드 3계층 구조 세팅

--- src/server/routers/products.js
+++ src/server/routers/products.js
@@ -1,9 +1,58 @@
-const express = require('express');
-const router = express.Router();
-const { Product } = require('../db');
+const { Router } = require('express');
+const { productService } = require('../services/productService');
+const asyncHandler = require('../utils/async-handler');
 
-router.get('/', function (req, res, next) {
-    res.json({ result: 'products' });
-});
+const router = Router();
+
+router.post(
+    '/',
+    asyncHandler(async (req, res, next) => {
+        const newProduct = req.body;
+        const result = await productService.addProduct(newProduct);
+        res.status(201).json({
+            code: 201,
+            message: '상품 등록이 완료되었습니다.',
+            data: result,
+        });
+    })
+);
+
+router.get(
+    '/',
+    asyncHandler(async (req, res, next) => {
+        const result = await productService.findAllProducts();
+        res.status(200).json({
+            code: 200,
+            message: '요청이 성공적으로 완료되었습니다.',
+            data: result,
+        });
+    })
+);
+
+router.get(
+    '/:id',
+    asyncHandler(async (req, res, next) => {
+        const _id = req.params.id;
+        const result = await productService.findProduct(_id);
+        res.status(200).json({
+            code: 200,
+            message: '요청이 성공적으로 완료되었습니다.',
+            data: result,
+        });
+    })
+);
+
+router.delete(
+    '/:id',
+    asyncHandler(async (req, res, next) => {
+        const _id = req.params.id;
+        const result = await productService.removeProduct(_id);
+        res.status(200).json({
+            code: 200,
+            message: '요청이 성공적으로 완료되었습니다.',
+            data: result,
+        });
+    })
+);
 
 module.exports = router;
