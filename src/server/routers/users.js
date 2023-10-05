8fabd3875e337d0afe476f37bb963eac52903b99
[SW6] 박제성 <richdad6208@gmail.com>
2023-10-06 04:54:45 +0900

    refactor: 회원가입 로그인 로그아웃 api userRouter 폴더로 이동

--- src/server/routers/users.js
+++ src/server/routers/users.js
@@ -2,8 +2,76 @@ const express = require('express');
 const router = express.Router();
 const { orderService } = require('../services/orderService');
 const asyncHandler = require('../utils/async-handler');
+const { userService } = require('../services/userService');
 
 /* GET users listing. */
+router.post(
+    '/register',
+    asyncHandler(async (req, res, next) => {
+        const { email, password } = req.body;
+
+        if (email.length === 0 || password.length === 0) {
+            throw Object.assign(new Error('이메일 혹은 패스워드를 입력해주세요'), { status: 400 });
+        }
+
+        const checkUser = await userService.findOneUser(email);
+        if (checkUser) {
+            throw Object.assign(new Error('이미 등록된 메일입니다'), { status: 400 });
+        }
+
+        // TODO: 원할한 테스트를 위해 잠시 주석처리 하겠습니다
+        // const regExp = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,16}$/;
+
+        // if (!regExp.test(password)) {
+        //     throw Object.assign(new Error('비밀번호 조건에 맞지 않습니다'), { status: 400 });
+        // }
+
+        const result = await userService.signUpUser({ email, password });
+        res.status(201).json({
+            code: 201,
+            message: '회원가입이 완료되었습니다',
+            data: result,
+        });
+    })
+);
+
+router.post(
+    '/login',
+    asyncHandler(async (req, res, next) => {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            throw Object.assign(new Error('이메일 혹은 패스워드를 입력해주세요'), { status: 400 });
+        }
+
+        const checkedUser = await userService.findOneUser(email);
+
+        if (!checkedUser) {
+            throw Object.assign(new Error('이메일 혹은 패스워드가 일치하지 않습니다'), { status: 400 });
+        }
+
+        const result = await userService.signInUser(checkedUser, password, res);
+        res.status(200).json({
+            code: 200,
+            message: '로그인이 완료되었습니다',
+            data: { email: result },
+        });
+    })
+);
+
+router.post(
+    '/logout',
+    asyncHandler(async (req, res, next) => {
+        res.clearCookie('authorization');
+
+        res.status(200).json({
+            code: 200,
+            message: '로그아웃이 완료되었습니다',
+            data: null,
+        });
+    })
+);
+
 router.get(
     '/',
     asyncHandler(async (req, res, next) => {
