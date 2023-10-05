fb7eb44c33d02d39595e93d4968afb4ba4759fa1
[SW6] 박제성 <richdad6208@gmail.com>
2023-10-05 20:15:10 +0900

    feat: 주문 api 중 배송지 있을때 배송지 조회

--- src/server/routers/orders.js
+++ src/server/routers/orders.js
@@ -16,17 +16,9 @@ router.post(
                 console.log('필수 배송정보를 입력해주세요');
             }
             delivery = await deliveryService.addDeliveryAndUpdateUser(req.body, loggedInUser._id);
+        } else {
+            delivery = await deliveryService.findDelivery(loggedInUser.delivery);
         }
-
-        // 배송 정보 확인/저장
-        // -> 기존에 배송정보 스키마에 있는 정보라면 id 값이 있음
-        // -> 없으면 배송정보 스키마에 신규 등록
-
-        // 주문 내역 저장
-        // -> 배송 정보, 상품 정보 입력
-        // -> 저장
-        // 배송 정보 ID
-        // const result = await productService.addProduct({ newProduct, contentFile });
 
         res.status(201).json({
             code: 201,
