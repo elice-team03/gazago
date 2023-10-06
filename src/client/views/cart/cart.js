2416f90777b0bdab44cb4f0185ae483a836b3edf
윤보영 <qhdud4357@gmail.com>
2023-10-06 22:05:08 +0900

    feat: 장바구니 총 결제금액 계산 기능 구현

--- src/client/views/cart/cart.js
+++ src/client/views/cart/cart.js
@@ -3,18 +3,27 @@ const prices = document.querySelectorAll('.counter__price');
 const plusBtns = document.querySelectorAll('.counter__plus');
 const minusBtns = document.querySelectorAll('.counter__minus');
 const quantities = document.querySelectorAll('.counter__number');
+const totalProductPrice = document.getElementById('total__price--product');
 
+//상품 가격을 계산하는 함수
 function calculateProductPrice(idx) {
     const numberPrice = Number(originalPrices[idx].innerHTML.replace('원', '').replace(',', ''));
     const result = numberPrice * Number(quantities[idx].value);
     prices[idx].innerHTML = `${result.toLocaleString()}원`;
 }
+//각 플러스 버튼마다 클릭이 발생하면 각 idx에 해당하는 quantity가 1씩 증가하고, 상품가격 계산 함수가 호출됨
 plusBtns.forEach((item, idx) => {
     item.addEventListener('click', () => {
         quantities[idx].value++;
         calculateProductPrice(idx);
+        if (totalProductPrice.innerHTML !== '0') {
+            //총 주문금액이 0원 이상일 때(checkbox가 하나라도 선택됐을 때)
+            calculateTotalPrice(idx); //상품금액이 변경됐을 때의 총 주문금액을 계산하는 함수를 호출
+        }
     });
 });
+//각 마이너스 버튼마다 클릭이 발생하면 각 idx에 해당하는 quantity가 1씩 감소하고, 상품가격 계산 함수가 호출됨
+//quantity가 1보다 작을 시 최소구매 수량 경고가 발생하고 quantity가 1로 변경됨
 minusBtns.forEach((item, idx) => {
     item.addEventListener('click', () => {
         const quantity = quantities[idx];
@@ -25,8 +34,14 @@ minusBtns.forEach((item, idx) => {
             return;
         }
         calculateProductPrice(idx);
+        if (totalProductPrice.innerHTML !== '0') {
+            //총 주문금액이 0원 이상일 때(checkbox가 하나라도 선택됐을 때)
+            calculateTotalPrice(idx); //상품금액이 변경됐을 때의 총 주문금액을 계산하는 함수를 호출
+        }
     });
 });
+//사용자가 input을 통해 quantity를 수정하면 상품가격 계산 함수가 호출됨
+//quantity가 1보다 작을 시 최소구매 수량 경고가 발생하고 quantity가 1로 변경됨
 quantities.forEach((item, idx) => {
     item.addEventListener('change', () => {
         const quantity = quantities[idx];
@@ -35,5 +50,44 @@ quantities.forEach((item, idx) => {
             quantity.value = 1;
         }
         calculateProductPrice(idx);
+        if (totalProductPrice.innerHTML !== '0') {
+            //총 주문금액이 0원 이상일 때(checkbox가 하나라도 선택됐을 때)
+            calculateTotalPrice(idx);
+        }
+    });
+});
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const deliveryPrice = document.getElementById('total__price--delivery');
+const totalPrice = document.getElementById('total__price');
+
+function calculateTotalPrice() {
+    let result = 0;
+    let delivery = 0;
+    checkboxes.forEach((item, idx) => {
+        if (idx !== 0) {
+            const numberPrice = Number(prices[idx - 1].innerHTML.replace('원', '').replace(',', ''));
+            if (item.checked === true) {
+                result += numberPrice;
+                if (result < 80000) delivery = 3000;
+                else delivery = 0;
+            }
+        }
+    });
+    totalProductPrice.innerHTML = result.toLocaleString();
+    deliveryPrice.innerHTML = delivery.toLocaleString();
+    totalPrice.innerHTML = (result + delivery).toLocaleString();
+}
+//전체선택 눌렀을 때 모든 체크박스가 선택되는 이벤트
+function selectAll(selectAll) {
+    checkboxes.forEach((checkbox) => {
+        checkbox.checked = selectAll.checked;
+    });
+    calculateTotalPrice();
+}
+//체크박스를 하나씩 선택 or 해제할 때 실행되는 이벤트
+checkboxes.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        calculateTotalPrice();
+        if (!e.target.checked) checkboxes[0].checked = false;
     });
 });
