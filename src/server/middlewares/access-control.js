cb44b12ba8c99eb40f5f6f94f206964b433f3c58
[SW6] 박제성 <richdad6208@gmail.com>
2023-10-09 06:05:00 +0900

    feat: 권한별 페이지 접근통제 미들웨어 작성 및 라우터에 적용

--- src/server/middlewares/access-control.js
+++ src/server/middlewares/access-control.js
@@ -1,5 +1,6 @@
-const allowOnlyUser = (req, res, next) => {
-    if (req.user?.user.role !== 'users') {
+const allowUser = (req, res, next) => {
+    const role = req.user?.user.role || null;
+    if (role !== 'user') {
         return res.send(
             "<script>alert('로그인이 필요한 페이지입니다');location.href='http://localhost:5001/login';</script>"
         );
@@ -8,8 +9,31 @@ const allowOnlyUser = (req, res, next) => {
     return;
 };
 
+const allowAdmin = (req, res, next) => {
+    const role = req.user?.user.role || null;
+    if (role !== 'admin') {
+        return res.send(
+            "<script>alert('접근 권한이 없습니다. 분하쥬');location.href='http://localhost:5001';</script>"
+        );
+    }
+    next();
+    return;
+};
+
 const allowUserAndAdmin = (req, res, next) => {
-    if (req.user?.user.role !== 'users' || req.user?.user.role !== 'admin') {
+    const role = req.user?.user.role || null;
+    if (role !== 'user' && role !== 'admin') {
+        return res.send(
+            "<script>alert('로그인이 필요한 페이지입니다');location.href='http://localhost:5001/login';</script>"
+        );
+    }
+    next();
+    return;
+};
+
+const allowOrdererNotUserAndUserAndAdmin = (req, res, next) => {
+    const role = req.user?.user.role || null;
+    if (role !== 'ordererNotUser' && role !== 'user' && role !== 'admin') {
         return res.send(
             "<script>alert('로그인이 필요한 페이지입니다');location.href='http://localhost:5001/login';</script>"
         );
@@ -19,17 +43,18 @@ const allowUserAndAdmin = (req, res, next) => {
 };
 
 const blockUserAndAdmin = (req, res, next) => {
-    if (req.user?.user.role !== 'users' || req.user?.user.role !== 'admin') {
-        return res.send(
-            "<script>alert('로그인이 필요한 페이지입니다');location.href='http://localhost:5001/login';</script>"
-        );
+    const role = req.user?.user.role || null;
+    if (role === 'user' || role === 'admin') {
+        return res.send("<script>alert('잘못된 접근입니다');location.href='http://localhost:5001';</script>");
     }
     next();
     return;
 };
 
 module.exports = {
-    allowOnlyUser,
+    allowUser,
+    allowAdmin,
     allowUserAndAdmin,
+    allowOrdererNotUserAndUserAndAdmin,
     blockUserAndAdmin,
 };
