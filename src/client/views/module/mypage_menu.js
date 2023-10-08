f51f072250caad9752f9d7bc274a3547b6fe6a03
SungHyun Oh <lessbutbttr@gmail.com>
2023-10-08 23:00:23 +0900

    Fix: 마이페이지 모듈화 수정

--- src/client/views/module/mypage_menu.js
+++ src/client/views/module/mypage_menu.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     // fetch를 사용하여 mypage_menu.html 파일을 가져오기.
-    fetch('mypage_menu.html')
+    fetch('/module/mypage_menu.html')
         .then((response) => response.text())
         .then((data) => {
             document.querySelector('#menu-container').innerHTML = data;
