5f12643a4e166913ffbea155bfb360e106f4486b
SungHyun Oh <lessbutbttr@gmail.com>
2023-10-08 23:24:19 +0900

    Fix: 공통 api.js patch, delete함수 추가

--- src/client/views/api.js
+++ src/client/views/api.js
@@ -28,7 +28,7 @@ async function get(endpoint, params = '') {
         const response = await fetch(apiUrl, {
             method: 'GET',
             headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                // Authorization: `Bearer ${localStorage.getItem('token')}`,
             },
         });
         const result = await response.json();
@@ -38,4 +38,44 @@ async function get(endpoint, params = '') {
     }
 }
 
-export { post, get };
+// API PATCH
+async function patch(endpoint, data) {
+    try {
+        const apiUrl = endpoint;
+        const dataJson = JSON.stringify(data);
+
+        const response = await fetch(apiUrl, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                // Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+            body: dataJson,
+        });
+        const result = await response.json();
+        return result;
+    } catch (error){
+        throw error;
+    }   
+}
+
+// API DELETE
+// delete는 js 예약어 중 하나로 deleteRequest로 명명
+async function deleteRequest(endpoint) {
+    try {
+        const apiUrl = endpoint;
+
+        const response = await fetch(apiUrl, {
+            method: 'DELETE',
+            headers: {
+                // Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+        });
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export { post, get, patch, deleteRequest };
