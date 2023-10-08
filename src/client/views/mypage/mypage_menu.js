3247138b9e3001b901d04049158a781b9d9a7a7f
SungHyun Oh <lessbutbttr@gmail.com>
2023-10-08 14:47:19 +0900

    Feat: 마이페이지 메뉴 모듈화 작업

---
+++ src/client/views/mypage/mypage_menu.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // fetch를 사용하여 mypage_menu.html 파일을 가져오기.
+  fetch('mypage_menu.html')
+      .then(response => response.text())
+      .then(data => {
+          
+          document.querySelector('#menu-container').innerHTML = data;
+      })
+      .catch(error => {
+          console.error("HTML 파일을 불러오는 중 오류가 발생했습니다.", error);
+      });
+});
