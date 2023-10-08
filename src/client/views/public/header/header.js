d2e6d0dbb8cfd110b7b5eed613e901294d3b9629
Tiger Water <ddddf123@gmail.com>
2023-10-09 01:09:33 +0900

    fix: 상품목록 라우터 수정, 타이틀 변경, 헤더 푸터 텍스트화, 함수 수정

---
+++ src/client/views/public/header/header.js
@@ -0,0 +1,50 @@
+const headerContent = `<header class="header container">
+<div class="top-inner">
+    <ul class="user-login">
+        <li><a href="#">로그인</a></li>
+        <li><a href="#">회원가입</a></li>
+    </ul>
+</div>
+<div class="header-main">
+    <div class="logo">
+        <img src="logo.png" alt="Logo">
+    </div>
+    <form action="#" method="GET" class="search">
+            <input name="q" class="input is-normal is-rounded" type="text" placeholder="검색">
+            <button class="button" type="submit">검색</button>
+    </form>
+    <ul class="user-cart-mypage">
+        <li><a href="#">장바구니</a></li>
+        <li><a href="#">마이페이지</a></li>
+    </ul>
+</div>
+<nav class="navbar-container main-nav">
+    <ul class="navbar">
+        <li><a class="navbar-item" href="#">텐트/타프</a></li>
+        <li><a class="navbar-item" href="#">침낭/매트</a></li>
+        <li><a class="navbar-item" href="#">퍼니쳐</a></li>
+        <li><a class="navbar-item" href="#">주방/바베큐</a></li>
+        <li><a class="navbar-item" href="#">악세사리</a></li>
+    </ul>
+</nav>
+<nav class="navbar-container sub-nav">
+    <ul class="navbar">
+        <li><a class="navbar-item" href="#">텐트/타프</a></li>
+        <li><a class="navbar-item" href="#">침낭/매트</a></li>
+        <li><a class="navbar-item" href="#">퍼니쳐</a></li>
+        <li><a class="navbar-item" href="#">주방/바베큐</a></li>
+        <li><a class="navbar-item" href="#">악세사리</a></li>
+        <li><a class="navbar-item" href="#">텐트/타프</a></li>
+        <li><a class="navbar-item" href="#">침낭/매트</a></li>
+        <li><a class="navbar-item" href="#">퍼니쳐</a></li>
+        <li><a class="navbar-item" href="#">주방/바베큐</a></li>
+        <li><a class="navbar-item" href="#">악세사리</a></li>
+        
+    </ul>
+</nav>
+</header>`
+const header = document.querySelector('header');
+function insertHeader() {
+    header.innerHTML = headerContent;
+}
+insertHeader();
