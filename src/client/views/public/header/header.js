6f405003bc33660e7bb1aa1ab55ef2c2ae11496c
[SW6]김범수 <ddddf123@gmail.com>
2023-10-09 02:36:09 +0000

    feature: 홈페이지 슬라이더 구현, 상품목록 렌더링 구현

--- src/client/views/public/header/header.js
+++ src/client/views/public/header/header.js
@@ -20,50 +20,26 @@ const headerContent = `<header class="header container">
 </div>
 <nav class="navbar-container main-nav">
     <ul class="navbar">
-        <li><a class="navbar-item" id="navbar-item-1" href="#">텐트/타프</a></li>
-        <li><a class="navbar-item" id="navbar-item-2" href="#">침낭/매트</a></li>
-        <li><a class="navbar-item" id="navbar-item-3" href="#">퍼니쳐</a></li>
-        <li><a class="navbar-item" id="navbar-item-4" href="#">주방/바베큐</a></li> 
-        <li><a class="navbar-item" id="navbar-item-5" href="#">악세사리</a></li>
+        <li><a class="navbar-item" href="#">텐트/타프</a></li>
+        <li><a class="navbar-item" href="#">침낭/매트</a></li>
+        <li><a class="navbar-item" href="#">퍼니쳐</a></li>
+        <li><a class="navbar-item" href="#">주방/바베큐</a></li>
+        <li><a class="navbar-item" href="#">악세사리</a></li>
     </ul>
 </nav>
-<nav class="navbar-container sub-nav" id="sub-nav1">
+<nav class="navbar-container sub-nav">
     <ul class="navbar">
-        <li><a class="navbar-item" href="#">텐트</a></li>
-        <li><a class="navbar-item" href="#">타프</a></li>
-        <li><a class="navbar-item" href="#">쉘터</a></li>
-    </ul>
-</nav>
-<nav class="navbar-container sub-nav" id="sub-nav2">
-    <ul class="navbar">
-        <li><a class="navbar-item" href="#">침낭</a></li>
-        <li><a class="navbar-item" href="#">에어매트</a></li>
-        <li><a class="navbar-item" href="#">블랑켓</a></li>
-        <li><a class="navbar-item" href="#">배개</a></li>
-    </ul>
-</nav>
-<nav class="navbar-container sub-nav" id="sub-nav3">
-    <ul class="navbar">
-        <li><a class="navbar-item" href="#">테이블</a></li>
-        <li><a class="navbar-item" href="#">체어</a></li>
-        <li><a class="navbar-item" href="#">야전침대</a></li>
-        <li><a class="navbar-item" href="#">해먹</a></li>
-    </ul>
-</nav>
-<nav class="navbar-container sub-nav" id="sub-nav4">
-    <ul class="navbar">
-        <li><a class="navbar-item" href="#">BBQ용품</a></li>
-        <li><a class="navbar-item" href="#">코펠/쿠커</a></li>
-        <li><a class="navbar-item" href="#">키친소품</a></li>
-        <li><a class="navbar-item" href="#">스토브</a></li>
-    </ul>
-</nav>
-<nav class="navbar-container sub-nav" id="sub-nav5">
-    <ul class="navbar">
-        <li><a class="navbar-item" href="#">렌턴</a></li>
-        <li><a class="navbar-item" href="#">장식용품</a></li>
-        <li><a class="navbar-item" href="#">캠핑연장</a></li>
-        <li><a class="navbar-item" href="#">샤워용품/세탁용품</a></li>
+        <li><a class="navbar-item" href="#">텐트/타프</a></li>
+        <li><a class="navbar-item" href="#">침낭/매트</a></li>
+        <li><a class="navbar-item" href="#">퍼니쳐</a></li>
+        <li><a class="navbar-item" href="#">주방/바베큐</a></li>
+        <li><a class="navbar-item" href="#">악세사리</a></li>
+        <li><a class="navbar-item" href="#">텐트/타프</a></li>
+        <li><a class="navbar-item" href="#">침낭/매트</a></li>
+        <li><a class="navbar-item" href="#">퍼니쳐</a></li>
+        <li><a class="navbar-item" href="#">주방/바베큐</a></li>
+        <li><a class="navbar-item" href="#">악세사리</a></li>
+        
     </ul>
 </nav>
 </header>`
