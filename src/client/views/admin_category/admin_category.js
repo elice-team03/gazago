b03955ea4900ebe603de60fc4e5f65bf3ffbc02f
kim seolhwa <seolhwa7898@gmail.com>
2023-10-09 06:43:02 +0000

    feat:관리자 - 카테고리 관리 조회, 등록,삭제 / 상품 관리 조회, 등록,삭제

--- src/client/views/admin_category/admin_category.js
+++ src/client/views/admin_category/admin_category.js
@@ -2,11 +2,11 @@ import * as Api from '../../api.js';
 import BulmaModal from '/js/admin/BulmaModal.js';
 
 const parentCategoryIdObj = {
-    '텐트/타프': '65254773ae3e0cff77e679bd',
-    침낭: '6525477eae3e0cff77e679bf',
-    퍼니처: '65254783ae3e0cff77e679c1',
-    '주방/바베큐': '65254788ae3e0cff77e679c3',
-    악세사리: '65254795ae3e0cff77e679c5',
+    '텐트/타프': '651e5fbcbdfd9dbe2858c888',
+    침낭: '',
+    퍼니처: '',
+    '주방/바베큐': '',
+    악세사리: '',
 };
 
 const category = {
@@ -29,7 +29,7 @@ const category = {
         const parentCategorySel = document.querySelector('#parentCategorySel');
         const childCategoryInp = document.querySelector('#childCategoryInp');
         try {
-            let response = await Api.post('/api/categories/', {
+            let response = await Api.post('http://localhost:5001/api/categories', {
                 name: childCategoryInp.value,
                 parentCategoryId: parentCategoryIdObj[parentCategorySel.value],
             });
@@ -57,12 +57,10 @@ const category = {
                 alert('수정 중 오류가 발생했습니다.');
             }
         } catch (err) {
-            alert(err.message);
+            alert(err?.response?.data?.message);
         }
     },
 };
-
-console.log(111, category);
 
 // 초기화 : 화면 접근시 데이터베이스에 접근해서 목록 가져오기
 const initialize = async () => {
@@ -82,7 +80,9 @@ const initialize = async () => {
                             element.parentCategory.name
                         }</td>
                         <td class="custom_text_align_center custom_vertical_align_middle">${element.name}</td>
-                        <td class="custom_text_align_center custom_vertical_align_middle">${item.productCount}</td>
+                        <td class="custom_text_align_center custom_vertical_align_middle">${
+                            element.count ? element.count : 10
+                        }</td>
                         <td class="custom_vertical_align_middle">
                             <div class="control custom_text_align_center">
                                 <label class="radio">
