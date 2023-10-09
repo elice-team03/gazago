bd749ee44ca25838d952075bc88d44b4939b3f61
[SW6] 박제성 <richdad6208@gmail.com>
2023-10-09 06:53:54 +0000

    feature: 유저정보 변경 API, 임시비밀번호 메일발송 기능, 유저 권한레벨에 따른 페이지 접근통제 미들웨어 구현

--- src/client/routers/views.js
+++ src/client/routers/views.js
@@ -14,13 +14,12 @@ viewsRouter.use('/mypage/wishlist', requiredLogin, serveStatic('mypage_wishlist'
 viewsRouter.use('/mypage/account', requiredLogin, serveStatic('mypage_account'));
 viewsRouter.use('/product/detail', serveStatic('product_detail'));
 viewsRouter.use('/product/list', serveStatic('product_list'));
-viewsRouter.use('/order', serveStatic('order_page'));
-viewsRouter.use('/order/result', serveStatic('order_result'));
-viewsRouter.use('/admin/category', serveStatic('admin_category'));
-viewsRouter.use('/admin/product', serveStatic('admin_product'));
-viewsRouter.use('/admin/order', serveStatic('admin_order'));
+viewsRouter.use('/order', requiredLogin, serveStatic('order_page'));
+viewsRouter.use('/order/result', requiredLogin, serveStatic('order_result'));
+viewsRouter.use('/admin/category', checkAdmin, serveStatic('admin_category'));
+viewsRouter.use('/admin/product', checkAdmin, serveStatic('admin_product'));
+viewsRouter.use('/admin/order', checkAdmin, serveStatic('admin_order'));
 viewsRouter.use('/module', serveStatic('module'));
-viewsRouter.use('/', serveStatic(''));
 
 function serveStatic(resource) {
     const resourcePath = path.join(__dirname, `../views/${resource}`);
