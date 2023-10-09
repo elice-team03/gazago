a74d4b73346ed8eccdb978b70512a35674766a07
[SW6] 박제성 <richdad6208@gmail.com>
2023-10-09 06:53:54 +0000

    feature: 유저정보 변경 API, 임시비밀번호 메일발송 기능, 유저 권한레벨에 따른 페이지 접근통제 미들웨어 구현

--- src/server/routers/users.js
+++ src/server/routers/users.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { orderService } = require('../services/orderService');
 const asyncHandler = require('../utils/async-handler');
 const { userService } = require('../services/userService');
 const { deliveryService } = require('../services/deliveryService');
-
 /** 회원가입 API */
 router.post(
     '/register',
@@ -20,14 +18,7 @@ router.post(
             throw Object.assign(new Error('이미 등록된 메일입니다'), { status: 400 });
         }
 
-        // TODO: 원할한 테스트를 위해 잠시 주석처리 하겠습니다
-        // const regExp = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,16}$/;
-
-        // if (!regExp.test(password)) {
-        //     throw Object.assign(new Error('비밀번호 조건에 맞지 않습니다'), { status: 400 });
-        // }
-
-        const result = await userService.signUpUser({ email, password });
+        const result = await userService.signUpUser({ email, password, res });
         res.status(201).json({
             code: 201,
             message: '회원가입이 완료되었습니다',
@@ -61,6 +52,27 @@ router.post(
     })
 );
 
+/** 로그인 확인 여부 API */
+router.get(
+    '/check',
+    asyncHandler(async (req, res, next) => {
+        console.log(req.user);
+        if (req.user) {
+            res.json({
+                code: 200,
+                message: '로그인 중입니다',
+                data: true,
+            });
+        } else {
+            res.json({
+                code: 200,
+                message: '로그아웃 중 입니다',
+                data: false,
+            });
+        }
+    })
+);
+
 /** 로그아웃 API */
 router.post(
     '/logout',
@@ -79,22 +91,29 @@ router.post(
 router.get(
     '/',
     asyncHandler(async (req, res, next) => {
-        if (!req.user) {
-            throw Object.assign(new Error('로그인이 필요합니다'), { status: 401 });
-        }
         const loggedInUser = req.user.user;
-        const { email, _id, wishList, delivery } = loggedInUser;
-        const orders = await orderService.findByOrderer(_id);
-        const deliveyInform = await deliveryService.findDeliveryById(delivery);
+        const id = loggedInUser._id;
+        const user = await userService.findUser(id);
+        const { _id, email, role, wishList, delivery, orders, updatedAt, createdAt } = user;
+        yInform = await deliveryService.findDeliveryById(delivery);
         res.json({
             code: 200,
             message: '요청이 성공하였습니다',
-            data: { email, _id, wishList, orders, deliveyInform },
+            data: {
+                _id,
+                email,
+                role,
+                wishList,
+                delivery,
+                orders,
+                updatedAt,
+                createdAt,
+            },
         });
     })
 );
 
-/** 회원정보 변경 API */
+/** 회원정보 변경 (비밀번호 제외) API */
 router.patch(
     '/',
     asyncHandler(async (req, res, next) => {
@@ -114,7 +133,35 @@ router.patch(
     })
 );
 
+/** 회원 비밀번호 변경 API */
+router.patch(
+    '/password',
+    asyncHandler(async (req, res, next) => {
+        const loggedInUser = req.user.user;
+        const { oldPassword, newPassword } = req.body;
 
+        await userService.changePassword({ oldPassword, newPassword, loggedInUser });
+        res.json({
+            code: 200,
+            message: '비밀번호 변경을 완료하였습니다',
+            data: null,
+        });
+    })
+);
 
+/** 회원 임시 비밀번호 발송 API */
+router.post(
+    '/password',
+    asyncHandler(async (req, res, next) => {
+        const { email } = req.body;
+        await userService.changePasswordAndSendByEmail(email);
+
+        res.status(200).json({
+            code: 200,
+            message: '임시 비밀번호가 이메일로 발송되었습니다',
+            data: null,
+        });
+    })
+);
 
 module.exports = router;
