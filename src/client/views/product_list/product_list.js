48a032180dcf5362ed9ed4a6e303d9e3f3c4c145
Tiger Water <ddddf123@gmail.com>
2023-10-10 16:18:05 +0900

    fix: get 함수 api url 수정

--- src/client/views/product_list/product_list.js
+++ src/client/views/product_list/product_list.js
@@ -1,9 +1,10 @@
 import * as Api from '../api.js';
 
-const params = window.location.search;
+const queryString = window.location.search;
+const params = queryString.substring(1);
 
 async function getPage(page, params) {
-    const response = await Api.get('http://127.0.0.1:5001/api',`products?page=${page}${params}`);
+    const response = await Api.get('http://127.0.0.1:5001/api',`products?page=${page}&${params}`);
     const data = response.data;
     return data;
 }
@@ -13,9 +14,6 @@ async function loadPage (page, params){
     const mappedData = productDataMapping(pageData);
     displayProducts(mappedData);
 };
-
-//첫페이지 불러오기
-loadPage(1, params);
 
 function productDataMapping(products) {
     return products.map((data) => {
@@ -29,6 +27,48 @@ function productDataMapping(products) {
         };
     });
 };
+
+
+
+const productContainer = document.querySelector('.product__container');
+
+const priceString = product => {
+    return product.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + ' 원';
+}
+
+function displayProducts(products) {
+    // 상품 목록을 돌면서 화면에 표시
+    products.forEach((product) => {
+        
+        const productBoxContent = `<div class="product-box">
+                                    <div class="product__img-box">
+                                        <a href="#">
+                                            <img class="product__img"
+                                                src="https://cdn-pro-web-251-115.cdn-nhncommerce.com/minimatr4152_godomall_com/data/goods/23/03/13/726/726_detail_027.jpg" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="brand__box-good">
+                                        <div class="brand__box">
+                                            
+                                                <span class="brand">${product.brand}</span>
+                                            
+                                        </div>
+                                        <div class="good-box">
+                                            <i class="fa-regular fa-bookmark" style="color: #8c8c8c;"></i>
+                                        </div>
+                                    </div>
+                                    <div class="product__name-box">
+                                        <a href="#">
+                                            <span class="product">${product.name}</span>
+                                        </a>
+                                    </div>
+                                    <div class="price__box">
+                                        <span class="price">${priceString(product)}</span>
+                                     </div>
+                                 </div>`;
+        productContainer.insertAdjacentHTML('beforeend',productBoxContent);
+    });
+}
 
 //무한스크롤
 const getNextPage = (()=>{
@@ -56,45 +96,6 @@ const getNextPage = (()=>{
     }
   });
 
-const productContainer = document.querySelector('.product__container');
-
-const priceString = product => {
-    return product.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + ' 원';
-}
-
-function displayProducts(products) {
-    // 상품 목록을 돌면서 화면에 표시
-    products.forEach((product) => {
-        
-        const productBoxContent = `<div class="product-box">
-                                    <div class="product__img-box">
-                                        <a href="#">
-                                            <img class="product__img"
-                                                src="https://cdn-pro-web-251-115.cdn-nhncommerce.com/minimatr4152_godomall_com/data/goods/23/03/13/726/726_detail_027.jpg" alt="">
-                                        </a>
-                                    </div>
-                                    <div class="brand__box-good">
-                                        <div class="brand__box">
-                                            <a href="#">
-                                                <span class="brand">${product.brand}</span>
-                                            </a>
-                                        </div>
-                                        <div class="good-box">
-                                            <i class="fa-regular fa-bookmark" style="color: #8c8c8c;"></i>
-                                        </div>
-                                    </div>
-                                    <div class="product__name-box">
-                                        <a href="#">
-                                            <span class="product">${product.name}</span>
-                                        </a>
-                                    </div>
-                                    <div class="price__box">
-                                        <span class="price">${priceString(product)}</span>
-                                     </div>
-                                 </div>`;
-        productContainer.insertAdjacentHTML('beforeend',productBoxContent);
-    });
-}
-
-
+//첫페이지 불러오기
+loadPage(1, params);
 
