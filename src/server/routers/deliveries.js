4ceb5752603fb1103cf2e1f0a24885c9df4f36a2
[SW6] 박제성 <richdad6208@gmail.com>
2023-10-10 06:11:54 +0000

    feat: 배송지 변경 API 구현 및 주문시 상세주소 추가

--- src/server/routers/deliveries.js
+++ src/server/routers/deliveries.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const asyncHandler = require('../utils/async-handler');
+const { userService } = require('../services/userService');
+const { deliveryService } = require('../services/deliveryService');
 
-router.post(
+router.patch(
     '/',
     asyncHandler(async (req, res, next) => {
-        const { title, code, address, contact, defaultYn } = req.body;
+        const { userId, newAddress } = req.body;
 
-        console.log(req.body);
+        const result = await deliveryService.changeAddress(userId, newAddress);
 
         res.json({
             code: 200,
-            message: '요청이 성공하였습니다',
-            data: null,
+            message: '주소 변경이 성공하였습니다',
+            data: result,
         });
     })
 );
