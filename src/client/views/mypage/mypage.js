cf232b2153dcb0f08496a65bdbde6a95d44f117d
SungHyun Oh <lessbutbttr@gmail.com>
2023-10-10 16:13:12 +0900

    Fix: 회원정보 조회 기능 수정

--- src/client/views/mypage/mypage.js
+++ src/client/views/mypage/mypage.js
@@ -1,12 +1,31 @@
 import * as Api from '/api.js';
 
+// 로그인 회원 정보 가져오는 함수
+async function getUserData() {
+  try {
+      const result = await Api.get('http://localhost:5001/api/users');
+      if (result.code === 200) {
+          const user = result.data;
+          console.log(user);
+
+      } else {
+          alert(result.message);
+      }
+  } catch (error) {
+      console.error('데이터를 가져오는 중 오류가 발생했습니다:', error);
+  }
+}
+getUserData();
+
 async function getUserOrderList () {
   try {
     const result = await Api.get('http://localhost:5001/api/users')
     if(result.code === 200) {
-      
+
     }
   } catch (error) {
     
   }
-}
+} 
+
+getUserOrderList();
--- src/client/views/mypage_account/mypage_account.js
+++ src/client/views/mypage_account/mypage_account.js
@@ -1,15 +1,14 @@
 import * as Api from '/api.js';
-import { getUserAddress } from './user_address.js';
 
 const userMailInput = document.querySelector('#userMail');
 // const authCodeInput = document.querySelector('#authCode');
 const phoneInput = document.querySelector('#phone');
-const address1Input = document.querySelector('#address1');
-const address2Input = document.querySelector('#address2');
-// const address3Input = document.querySelector('#address3');
-const editForm = document.querySelector('#form-container');
+const zipcodeInput = document.querySelector('#zipcode');
+const addressInput = document.querySelector('#address');
+const detailAddressInput = document.querySelector('#detailAddress');
+const editButton = document.querySelector('.edit-btn');
 
-// 화면 로드시 로그인 회원정보 불러오기
+// 화면 로드시 로그인한 회원정보 불러오는 함수
 async function getUserData() {
     try {
         const result = await Api.get('http://localhost:5001/api/users');
@@ -18,38 +17,32 @@ async function getUserData() {
             console.log(user);
             userMailInput.value = user.email;
             phoneInput.value = user.delivery.contact;
-            address1Input.value = user.delivery.code;
-            address2Input.value = user.delivery.address;
+            zipcodeInput.value = user.delivery.code;
+            addressInput.value = user.delivery.address;
+            detailAddressInput.value = user.delivery.subAddress;
         } else {
             alert(result.message);
         }
-        // address3Input.value = result.address3; 상세주소?
     } catch (error) {
         console.error('데이터를 가져오는 중 오류가 발생했습니다:', error);
     }
 }
 getUserData();
 
-// 회원 주소 등록
-// function editUserAdress() {
-//     const address1Input = document.querySelector('#address1');
-//     const address2Input = document.querySelector('#address2');
-//     getUserAddress();
-// }
-
 // 회원 정보 수정
-// async function editUserInfo() {
-//   try {
-//     const result = Api.patch('http://localhost:5001/api/users');
-//     if(result.code === 200) { 
+async function editUserInfo() {
+  try {
+    const result = Api.patch('http://localhost:5001/api/users');
+    if(result.code === 200) { 
+      alert('회원 정보 수정 완료');
+      // window.location.reload();
+    }
+  } catch (error) {
+    console.error('데이터를 가져오는 중 오류가 발생했습니다:', error);
+  }
+}
 
-//     }
-//   } catch (error) {
-//     console.error('데이터를 가져오는 중 오류가 발생했습니다:', error);
-//   }
-// }
-
-// editForm.addEventListener('submit', editUserInfo);
+editButton.addEventListener('click', editUserInfo);
 
 
 
