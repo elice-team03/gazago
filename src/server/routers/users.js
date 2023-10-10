15f54e4c41465ca94a2ad2af210fd1436e745f69
given02 <votmdnjem_id@naver.com>
2023-10-10 14:37:14 +0900

    feat: 사용자 위시리스트 추가 API 구현

--- src/server/routers/users.js
+++ src/server/routers/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const asyncHandler = require('../utils/async-handler');
+const { validateObjectId } = require('../utils/validate-utils');
 const { userService } = require('../services/userService');
 const { deliveryService } = require('../services/deliveryService');
+
 /** 회원가입 API */
 router.post(
     '/register',
@@ -13,7 +15,7 @@ router.post(
             throw Object.assign(new Error('이메일 혹은 패스워드를 입력해주세요'), { status: 400 });
         }
 
-        const checkUser = await userService.findOneUser(email);
+        const checkUser = await userService.findUserByEmail(email);
         if (checkUser) {
             throw Object.assign(new Error('이미 등록된 메일입니다'), { status: 400 });
         }
@@ -37,7 +39,7 @@ router.post(
             throw Object.assign(new Error('이메일 혹은 패스워드를 입력해주세요'), { status: 400 });
         }
 
-        const checkedUser = await userService.findOneUser(email);
+        const checkedUser = await userService.findUserByEmail(email);
 
         if (!checkedUser) {
             throw Object.assign(new Error('이메일 혹은 패스워드가 일치하지 않습니다'), { status: 400 });
@@ -113,6 +115,21 @@ router.get(
     })
 );
 
+/** 회원 임시 비밀번호 발송 API */
+router.post(
+    '/password',
+    asyncHandler(async (req, res, next) => {
+        const { email } = req.body;
+        await userService.changePasswordAndSendByEmail(email);
+
+        res.status(200).json({
+            code: 200,
+            message: '임시 비밀번호가 이메일로 발송되었습니다',
+            data: null,
+        });
+    })
+);
+
 /** 회원정보 변경 (비밀번호 제외) API */
 router.patch(
     '/',
@@ -149,17 +166,24 @@ router.patch(
     })
 );
 
-/** 회원 임시 비밀번호 발송 API */
-router.post(
-    '/password',
+router.patch(
+    '/wishlist',
     asyncHandler(async (req, res, next) => {
-        const { email } = req.body;
-        await userService.changePasswordAndSendByEmail(email);
+        const productId = req.body.productId;
+        const user = req.user.user;
+        if (!user) {
+            const error = new Error('로그인 후 이용 가능합니다.');
+            error.status = 400;
+            throw error;
+        }
 
-        res.status(200).json({
+        validateObjectId(productId, 400, '상품 정보를 확인해주세요.');
+        const result = await userService.addUserWishlist(user._id, productId);
+
+        res.status(201).json({
             code: 200,
-            message: '임시 비밀번호가 이메일로 발송되었습니다',
-            data: null,
+            message: '요청이 성공적으로 완료되었습니다.',
+            data: result,
         });
     })
 );
