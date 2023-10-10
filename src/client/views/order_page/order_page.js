b8bc57279be2d62d415b95a58c23c2e9f2c3375b
윤보영 <qhdud4357@gmail.com>
2023-10-10 19:00:34 +0900

    feat: 주문 페이지 주문상품 데이터 바인딩

--- src/client/views/order_page/order_page.js
+++ src/client/views/order_page/order_page.js
@@ -1,32 +1,53 @@
-const zipcodeButton = document.querySelector('.input__button--zipcode');
-function execDaumPostcode() {
-    new daum.Postcode({
-        oncomplete: function (data) {
-            let extraAddr = '';
-            if (data.userSelectedType === 'R') {
-                // 사용자가 도로명 주소를 선택했을 경우
-                // 법정동명이 있을 경우 참고항목에 추가한다. (법정리는 제외)
-                // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
-                if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
-                    extraAddr += data.bname;
-                }
-                // 건물명이 있고, 공동주택일 경우 추가한다.
-                if (data.buildingName !== '' && data.apartment === 'Y') {
-                    extraAddr += extraAddr !== '' ? ', ' + data.buildingName : data.buildingName;
-                }
-                // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
-                if (extraAddr !== '') {
-                    extraAddr = ' (' + extraAddr + ')';
-                }
-                // 조합된 참고항목을 도로명주소와 해당 필드에 넣는다.
-                document.getElementById('address').value = data.roadAddress + extraAddr;
-            } else {
-                // 사용자가 지번 주소를 선택했을 경우
-                document.getElementById('address').value = data.jibunAddress;
-            }
-            document.getElementById('zipcode').value = data.zonecode;
-            document.getElementById('detailAddress').focus();
-        },
-    }).open();
+import * as Api from '../api.js';
+const storage = window.localStorage;
+const orderData = JSON.parse(storage.getItem('order'));
+const productItem = document.querySelector('.product__item');
+let totalPrice = 0;
+
+const count = document.querySelector('.product__count');
+count.innerHTML = orderData.length;
+
+orderData.forEach(() => createOrderElement());
+orderData.forEach((item, idx) => {
+    renderOrderItems(item, idx);
+});
+
+function createOrderElement() {
+    productItem.innerHTML += `
+        <div class="product__detail">
+            <img class="product__image">
+            <div class="product__description">
+                <span class="product__text--brand"></span>
+                <span class="product__text--name"></span>
+                <span class="product__text--price"></span>
+            </div>
+        </div>
+    `;
 }
-zipcodeButton.addEventListener('click', execDaumPostcode);
+
+async function renderOrderItems(item, idx) {
+    const image = document.querySelectorAll('.product__image');
+    const brand = document.querySelectorAll('.product__text--brand');
+    const name = document.querySelectorAll('.product__text--name');
+    const price = document.querySelectorAll('.product__text--price');
+
+    const response = await Api.get('/api/products', item.id);
+    const data = response.data;
+
+    image[idx].setAttribute('src', data.thumbnailPath);
+    brand[idx].innerHTML = data.brand;
+    name[idx].innerHTML = data.name;
+    price[idx].innerHTML = `${data.price.toLocaleString()}원 / 수량 ${item.quantity}개`;
+    totalPrice += Number(data.price) * Number(item.quantity);
+    calculateTotalPrice(totalPrice);
+}
+function calculateTotalPrice(totalPrice) {
+    const productPrice = document.querySelector('.order__product');
+    const deliveryPrice = document.querySelector('.order__delivery');
+    const orderTotal = document.querySelector('.order__total');
+    let delivery = 0;
+    productPrice.innerHTML = `${totalPrice.toLocaleString()}원`;
+    if (totalPrice < 80000) delivery = 3000;
+    deliveryPrice.innerHTML = `+ ${delivery.toLocaleString()}원`;
+    orderTotal.innerHTML = `${(totalPrice + delivery).toLocaleString()}원`;
+}
