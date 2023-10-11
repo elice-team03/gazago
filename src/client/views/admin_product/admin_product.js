0c1690dbaa51a7df90155d44bc8aa05e736a7b39
kim seolhwa <seolhwa7898@gmail.com>
2023-10-11 15:33:00 +0000

    feat:관리자 - 주문 관리 조회, 주문상태 변경, 주문 취소, 필터 검색

--- src/client/views/admin_product/admin_product.js
+++ src/client/views/admin_product/admin_product.js
@@ -1,6 +1,94 @@
 import * as Api from '../../api.js';
 import BulmaModal from '/js/admin/BulmaModal.js';
 
+const parentCategoryOrder = { '텐트/타프': 0, 침낭: 1, 퍼니처: 2, '주방/바베큐': 3, 악세사리: 4 };
+let childCategoryOrder = {};
+
+const parentCategoryIdArr = [
+    { '텐트/타프': '65254773ae3e0cff77e679bd' },
+    { 침낭: '6525477eae3e0cff77e679bf' },
+    { 퍼니처: '65254783ae3e0cff77e679c1' },
+    { '주방/바베큐': '65254788ae3e0cff77e679c3' },
+    { 악세사리: '65254795ae3e0cff77e679c5' },
+];
+
+const parentCategorySelect = document.querySelector('#parentCategorySelect');
+
+const getParentOption = (parentCategorySelect, total = true) => {
+    let getparentCategoryKeyword = total ? [`<option value="">전체보기</option>`] : [];
+    parentCategoryIdArr.forEach((item) => {
+        let [key, value] = Object.entries(item)[0];
+        getparentCategoryKeyword.push(`<option value="${value}">${key}</option>`);
+    });
+    parentCategorySelect.innerHTML = getparentCategoryKeyword.join('');
+};
+
+const getOption = async (parentCategorySelect, childCategorySelector, total = true) => {
+    let parentCategorySelectValue = parentCategorySelect.value;
+
+    let endpoint = '/api/categories/menu?';
+    let params = `parentCategoryId=${parentCategorySelectValue}`;
+    let apiUrl = `${endpoint}${params}`;
+
+    const categoryRes = await fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+            // Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+    });
+
+    const categoryResult = await categoryRes.json();
+    if (categoryResult.code == 200) {
+        childCategoryOrder = {};
+        if (categoryResult != null || categoryResult.data != null) {
+            let categoryList = categoryResult.data;
+            let categoryObjList = [];
+            categoryList.forEach((item, idx) => {
+                let categoryObj = {};
+                categoryObj[item.name] = item._id;
+                categoryObjList.push(categoryObj);
+                childCategoryOrder[item.name] = idx;
+            });
+
+            let getCategoryKeyword = total ? [`<option value="" selected>전체보기</option>`] : [];
+            if (parentCategorySelectValue == '') {
+            } else {
+                categoryObjList.forEach((item, idx) => {
+                    let [key, value] = Object.entries(item)[0];
+                    getCategoryKeyword.push(`<option value="${value}">${key}</option>`);
+                });
+            }
+            document.querySelector(`#${childCategorySelector}`).innerHTML = getCategoryKeyword.join('');
+        }
+    }
+    return true;
+    // 선택된 중분류 값 추출
+    // api 호출해서 중분류 id 값 하위에 있는 소분류 값을 가져온다
+    // 가져온 값을 위에 리스트 형태로 변환한다
+};
+parentCategorySelect.addEventListener('change', () => {
+    getOption(parentCategorySelect, 'categorySelect');
+});
+const modalParentCategorySel = document.querySelector('.modal #parentCategorySel');
+modalParentCategorySel.addEventListener('change', () => {
+    getOption(modalParentCategorySel, 'childCategorySel', false);
+});
+getParentOption(parentCategorySelect);
+getOption(parentCategorySelect, 'categorySelect');
+
+const dateformat = (date) => {
+    const dateArr = [date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()];
+
+    dateArr.forEach((item, idx, arr) => {
+        if (item <= 9) {
+            arr[idx] = '0' + item;
+        }
+    });
+
+    let stringDate = `${date.getFullYear()}년${dateArr[0]}월${dateArr[1]}일 ${dateArr[2]}:${dateArr[3]}:${dateArr[4]}`;
+    return stringDate;
+};
+
 let isUpdate = false;
 
 const product = {
@@ -63,16 +151,46 @@ const product = {
 
         return null;
     },
+    getProductList: async () => {
+        let productNameInput = document.querySelector('#productNameInput').value;
+        let queryStringList = [];
+        if (isStringValue(productNameInput)) {
+            productNameInput = productNameInput.trim();
+            queryStringList.push('searchKeyword=' + productNameInput);
+        }
+
+        let queryString = queryStringList.join('&');
+        if (isStringValue(queryString)) {
+            queryString = '?' + queryString;
+        }
+
+        const endpoint = '/api/products';
+        const params = queryString;
+        const apiUrl = `${endpoint}${params}`;
+        // HTTP GET 요청 보내기
+        const response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                // Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+        });
+        const result = await response.json();
+        return result;
+    },
+};
+
+const isStringValue = (val) => {
+    return !!val?.trim();
 };
 
 const initialize = async () => {
     try {
-        let res = await Api.get('/api/products');
+        let res = await product.getProductList();
         if (res.code == 200) {
             if (res != null && res.data != null) {
                 let productList = res.data;
                 let tbody = document.querySelector('#product_list_tbody');
-
+                tbody.innerHTML = '';
                 productList.forEach((item, idx) => {
                     const element = item;
                     let tempRow = document.createElement('tr');
@@ -86,12 +204,14 @@ const initialize = async () => {
                                         </figure>
                                     </td>
                                     <td class="custom_text_align_center custom_vertical_align_middle is-size-7">
-                                        ${element.createdAt}
+                                        ${dateformat(new Date(element.createdAt))}
                                     </td>
                                     <td class="custom_text_align_center custom_vertical_align_middle is-size-7">${
-                                        element.category
+                                        element.category.parentCategory.name
                                     }</td>
-                                    <td class="custom_text_align_center custom_vertical_align_middle is-size-7">${'소분류 수정해야함'}</td>
+                                    <td class="custom_text_align_center custom_vertical_align_middle is-size-7">${
+                                        element.category.name
+                                    }</td>
                                     <td class="custom_text_align_center custom_vertical_align_middle is-size-7">${
                                         element.brand
                                     }</td>
@@ -101,8 +221,8 @@ const initialize = async () => {
                                     <td class="custom_text_align_center custom_vertical_align_middle is-size-7">${
                                         element.price
                                     }</td>
-                                    <td class="custom_text_align_center custom_vertical_align_middle is-size-7">${'누적 판매량 수정해야함'}</td>
-                                    <td class="custom_text_align_center custom_vertical_align_middle is-size-7">${'판매상태 수정해야함'}</td>
+                                    <td class="custom_text_align_center custom_vertical_align_middle is-size-7">${'누적 판매량'}</td>
+                                    <td class="custom_text_align_center custom_vertical_align_middle is-size-7">${'판매상태'}</td>
                                     <td class="custom_text_align_center custom_vertical_align_middle">
                                         <button id="prodeuct_btn_upd${idx}" class="button is-rounded is-small custom_background_color_prime">수정</button>
                                         <button id="prodeuct_btn_del${idx}" class="button is-rounded is-small custom_background_color_red">삭제</button>
@@ -112,7 +232,9 @@ const initialize = async () => {
                     const productRowUpdateButton = tempRow.querySelector(`#prodeuct_btn_upd${idx}`);
                     productRowUpdateButton.addEventListener('click', (e) => {
                         document.querySelector('#modalTitleId').innerText = '상품수정';
+                        document.querySelector('#product_btn_save').innerText = '수정하기';
                         isUpdate = true;
+
                         setDataInModal(element._id);
                     });
                     const productRowDeleteButton = tempRow.querySelector(`#prodeuct_btn_del${idx}`);
@@ -139,29 +261,54 @@ productBtnAdd.addEventListener('click', function () {
 });
 
 modal.addEventListener('modal:close', function () {
-    // const parentCategorySel = document.querySelector('#parentCategorySel')
-    // parentCategorySel.options[parentCategorySel.options.selectedIndex].selected = true
-    // const childCategorySel = document.querySelector('#childCategorySel')
-    // childCategorySel.options[childCategorySel.options.selectedIndex].selected = true
-    document.querySelector('#brandInput').value = '';
-    document.querySelector('#productNameInput').value = '';
-    document.querySelector('#priceInput').value = '';
-    document.querySelector('#thumbnailInput').value = '';
+    const parentCategorySel = document.querySelector('.modal #parentCategorySel');
+    parentCategorySel.options[parentCategorySel.options.selectedIndex].selected = true;
+    const childCategorySel = document.querySelector('.modal #childCategorySel');
+    childCategorySel.options[childCategorySel.options.selectedIndex].selected = true;
+    document.querySelector('.modal #brandInput').value = '';
+    document.querySelector('.modal #productNameInput').value = '';
+    document.querySelector('.modal #priceInput').value = '';
+    document.querySelector('.modal #thumbnailInput').value = '';
     // document.querySelector('#contentFile');
 });
 
 const setDataInModal = async (id) => {
     let productInfo = await product.productGetById(id);
-    document.querySelector('#productId').value = productInfo._id;
-    // document.querySelector('#parentCategorySel').value = productInfo.category.parentCategory.name;
-    // document.querySelector('#childCategorySel').value = productInfo.category.name ;
-    document.querySelector('#brandInput').value = productInfo.brand;
-    document.querySelector('#productNameInput').value = productInfo.name;
-    document.querySelector('#priceInput').value = productInfo.price;
-    document.querySelector('#thumbnailInput').value = productInfo.thumbnailPath;
-    // document.querySelector('#contentFile');
-    modal.show();
+    if (productInfo != null) {
+        const modalParentCategorySel = document.querySelector('.modal #parentCategorySel');
+        getParentOption(modalParentCategorySel, false);
+        document.querySelector('.modal #productId').value = productInfo._id;
+
+        document.querySelector('.modal #parentCategorySel').options[
+            parentCategoryOrder[productInfo.category.parentCategory.name]
+        ].selected = true;
+
+        const isEnd = await getOption(modalParentCategorySel, 'childCategorySel', false);
+        if (isEnd) {
+            document.querySelector('.modal #childCategorySel').options[
+                childCategoryOrder[productInfo.category.name]
+            ].selected = true;
+        }
+
+        document.querySelector('.modal #brandInput').value = productInfo.brand;
+        document.querySelector('.modal #productNameInput').value = productInfo.name;
+        document.querySelector('.modal #priceInput').value = productInfo.price;
+        document.querySelector('.modal #thumbnailInput').value = productInfo.thumbnailPath;
+        // document.querySelector('#contentFile');
+        modal.show();
+    }
 };
 
 const productModalSaveButton = document.querySelector('#product_btn_save');
 productModalSaveButton.addEventListener('click', product.saveInModal);
+
+const productSearchButton = document.querySelector('#searchButton');
+productSearchButton.addEventListener('click', initialize);
+
+const customInputEnter = document.querySelector('.custom_input_enter');
+customInputEnter.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        initialize();
+    }
+});
