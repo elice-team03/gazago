5d455702c03c9b60f708ab03f01b695ca52b4add
[SW6] 박제성 <richdad6208@gmail.com>
2023-10-11 17:31:30 +0900

    refactor: 이메일인증 공통함수 구현

--- src/server/services/userService.js
+++ src/server/services/userService.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const setUserToken = require('../utils/jwt');
 const generateRandomPasswrod = require('../utils/generate-password');
 const nodemailer = require('nodemailer');
+const { sendEmail } = require('../utils/send-email');
 
 class userService {
     static async addUser(newUser) {
@@ -97,75 +98,9 @@ class userService {
         }
         await User.updateOne({ email: email }, { password: newHashedPassword });
 
-        const transport = nodemailer.createTransport({
-            service: 'Gmail',
-            auth: {
-                user: process.env.NODEMAILER_ID,
-                pass: process.env.NODEMAILER_SECRET,
-            },
-        });
+        sendEmail('resetPassword', email, newPassword);
 
-        /** 임시 비밀번호 발송하는 메일형식 html inline style */
-        const mail = {
-            from: process.env.NODEMAILER_ID,
-            to: email,
-            subject: `[GAZAGO] 유저님의 비밀번호가 초기화 되었습니다`,
-            html: `
-            <div style="width: 500px; margin: auto; text-align: center">
-                <div style="text-align: center">
-                    <img src="https://i.imgur.com/onyitJ3.png" alt="" style="width: 200px" />
-                </div>
-                <div style="text-align: left; margin-top: 2rem">
-                    <p style="text-align: center; font-size: 30px; font-weight: bold; margin-bottom: 30px">
-                        임시 비밀번호 발급
-                    </p>
-                    <hr style="background: #395144; height: 3px; border: 0" />
-                    <p
-                        style="
-                            font-size: 18px;
-                            width: 32ch;
-                            text-align: left;
-                            word-spacing: -1px;
-                            line-height: 1.1;
-                            font-weight: bold;
-                            word-break: keep-all;
-                        "
-                    >
-                        임시 비밀번호로 로그인 하신 후에, 안전한 비밀번호로 변경 부탁드립니다.
-                    </p>
-                    <p style="text-indent: 10px; font-size: 18px; margin-top: 40px">임시 비밀번호</p>
-                    <p style="border-bottom: 1px solid lightgrey; padding-bottom: 5px">${newPassword}</p>
-                    <a
-                        href="http://localhost:5001/login"
-                        style="
-                            display: block;
-                            width: 100%;
-                            background: #004225;
-                            height: 45px;
-                            line-height: 45px;
-                            border-radius: 100vmax;
-                            color: white;
-                            text-decoration: none;
-                            font-weight: bold;
-                            text-align: center;
-                            margin-top: 2em;
-                            font-size: 18px;
-                        "
-                    >
-                        로그인 하러가기
-                    </a>
-                </div>
-            </div> 
-            `,
-        };
-
-        return transport.sendMail(mail, (error) => {
-            if (error) {
-                throw Object.assign(new Error('이메일 발송이 실패하였습니다'), { status: 400 });
-            }
-            transport.close();
-            return;
-        });
+        return;
     }
 
     /** 비밀번호 변경. */
