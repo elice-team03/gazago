7cb982598ccbaf547862a11c24a05cbe25bf2411
윤보영 <qhdud4357@gmail.com>
2023-10-14 09:18:59 +0900

    feat: 관리자 페이지네이션 추가, 수정

--- src/client/views/admin_product/admin_product.js
+++ src/client/views/admin_product/admin_product.js
@@ -1,5 +1,5 @@
 import * as Api from '../../api.js';
-import BulmaModal from '/js/admin/BulmaModal.js';
+import BulmaModal from '/js/admin/bulmaModal.js';
 
 const parentCategoryOrder = { '텐트/타프': 0, 침낭: 1, 퍼니처: 2, '주방/바베큐': 3, 악세사리: 4 };
 let childCategoryOrder = {};
@@ -231,9 +231,9 @@ const renderPagination = (totalPages) => {
             paginationLink[idx].classList.add('is-current');
             currentIndex = idx;
             product.getProductList();
-            initialize('none');
-        })
-    })
+            initialize();
+        });
+    });
     const nextPageButton = document.querySelector('.pagination-next');
     nextPageButton.addEventListener('click', () => {
         if(currentIndex + 1 < totalPages) {
@@ -241,7 +241,7 @@ const renderPagination = (totalPages) => {
             paginationLink[currentIndex + 1].classList.add('is-current');
             currentIndex++;
             product.getProductList();
-            initialize('none');
+            initialize();
         }
     })
     const previousPageButton = document.querySelector('.pagination-previous');
@@ -251,11 +251,11 @@ const renderPagination = (totalPages) => {
             paginationLink[currentIndex - 1].classList.add('is-current');
             currentIndex--;
             product.getProductList();
-            initialize('none');
+            initialize();
         }
     })
 };
-const initialize = async (recall) => {
+const initialize = async (render, recall) => {
     try {
         let res = await product.getProductList(1);
         if (res.code == 200) {
@@ -319,14 +319,19 @@ const initialize = async (recall) => {
 
                     tbody.append(tempRow);
                 });
-                if(recall !== 'none') renderPagination(data.totalPages);
+                if (render) {
+                    const paginationContainer = document.querySelector('.pagination-container');
+                    paginationContainer.innerHTML = '';
+                }
+                if (recall) renderPagination(data.totalPages);
+                
             }
         }
     } catch (err) {
         console.log(err);
     }
 };
-initialize();
+initialize(true, true);
 
 // modal
 
@@ -406,13 +411,13 @@ const productModalSaveButton = document.querySelector('#product_btn_save');
 productModalSaveButton.addEventListener('click', product.saveInModal);
 
 const productSearchButton = document.querySelector('#searchButton');
-productSearchButton.addEventListener('click', initialize);
+productSearchButton.addEventListener('click', () => initialize(true, true));
 
 const customInputEnter = document.querySelector('.custom_input_enter');
 customInputEnter.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
         e.preventDefault();
-        initialize();
+        initialize(true, true);
     }
 });
 
